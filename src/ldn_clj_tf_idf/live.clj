44eadd2bcbf35fe103e96d0f18813f135238178c
Tom O'Brien <thomas.obrien86@gmail.com>
2013-06-30 19:06:31 +0100

    Simple word count

--- src/ldn_clj_tf_idf/live.clj
+++ src/ldn_clj_tf_idf/live.clj
@@ -1,11 +1,13 @@
 (ns ldn-clj-tf-idf.live
-  (:require [cascalog.api :refer :all]))
+  (:require [cascalog.api :refer :all]
+            [cascalog.ops :as c]))
 
 #_(require '[cascalog.playground :refer :all])
 #_(bootstrap-emacs)
 
 ;; Cascalog speaks in tuples; the input to this query is a sequence of
-;; six 1-tuples.
+;; six 1-tuples. Cascalog comes with some built-in aggregators, the
+;; most simple being count, in the cascalog.ops namespace.
 #_(let [source [["Hello"]
                 ["London"]
                 ["Clojurians"]
@@ -13,5 +15,6 @@
                 ["Clojurians"]
                 ["worldwide"]]]
   (?- (stdout)
-      (<- [?word]
-          (source ?word))))
+      (<- [?word ?count]
+          (source ?word)
+          (c/count :> ?count))))
