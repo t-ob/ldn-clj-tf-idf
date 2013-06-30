fb49d2b2e0a061a096327779783192c6578e1aee
Tom O'Brien <thomas.obrien86@gmail.com>
2013-06-30 21:50:34 +0100

    Basic joins

--- src/ldn_clj_tf_idf/live.clj
+++ src/ldn_clj_tf_idf/live.clj
@@ -19,15 +19,17 @@
       (c/max ?freq :> ?max-freq)))
 
 ;; It's possible to read-in multiple sources to a Cascalog query, and
-;; join on specific variables. Let's calculate, for each document, the
-;; ratio of each occurring word to the document's maximum word
-;; frequency.
-#_(let [source         articles
-        word-freqs     (document-word-frequencies source)
-        max-word-freqs (document-max-word-frequencies word-freqs)]
+;; join on specific variables.
+#_(let [source-1 [["a" 1]
+                  ["a" 2]
+                  ["a" 3]
+                  ["b" 4]
+                  ["b" 5]
+                  ["c" 6]]
+        source-2 [["a" :foo]
+                  ["b" :bar]
+                  ["c" :baz]]]
     (?- (stdout)
-        (c/fixed-sample (<- [?label ?word ?term-frequency]
-                            (word-freqs ?label ?word ?freq)
-                            (max-word-freqs ?label ?max-freq)
-                            (div ?freq ?max-freq :> ?term-frequency))
-                        20)))
+        (<- [?x ?y ?z]
+            (source-1 ?x ?y)
+            (source-2 ?x ?z))))
