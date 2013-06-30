7cc75bed72db64a01b3e53e2a9848e9826119943
Tom O'Brien <thomas.obrien86@gmail.com>
2013-06-30 18:59:01 +0100

    Intro

--- src/ldn_clj_tf_idf/live.clj
+++ src/ldn_clj_tf_idf/live.clj
@@ -1,2 +1,14 @@
 (ns ldn-clj-tf-idf.live
   (:require [cascalog.api :refer :all]))
+
+#_(require '[cascalog.playground :refer :all])
+#_(bootstrap-emacs)
+
+;; Cascalog speaks in tuples; the input to this query is a sequence of
+;; three 1-tuples.
+#_(let [source [["Hello"]
+                ["London"]
+                ["Clojurians"]]]
+  (?- (stdout)
+      (<- [?word]
+          (source ?word))))
