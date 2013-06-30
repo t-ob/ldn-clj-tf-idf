fb5fbe38e69f23f7c7fd7722052b44a20da0c145
Tom O'Brien <thomas.obrien86@gmail.com>
2013-06-30 21:36:04 +0100

    Intro to joins

--- src/ldn_clj_tf_idf/live.clj
+++ src/ldn_clj_tf_idf/live.clj
@@ -12,25 +12,24 @@
       (source ?label ?word)
       (c/count :> ?freq)))
 
-;; The output of a Cascalog query is a perfectly valid input to
-;; another. articles is a query defined in samples.clj, which takes a
-;; set of news articles and transforms it into our desired tuple
-;; representation.
+(defn document-max-word-frequencies
+  [source]
+  (<- [?label ?max-freq]
+      (source ?label _ ?freq)
+      (c/max ?freq :> ?max-freq)))
 
-;; The fixed-sample query, defined in cascalog.ops returns a random
-;; sample of a source input, in this case another query. It's very
-;; handy when experimenting.
-#_(?- (stdout)
-      (c/fixed-sample articles
-                      10))
-
-;; Let's calculate the frequency of the most occurring word in each
-;; document, using the cascalog.ops/max aggregator.
-#_(let [source (document-word-frequencies articles)]
+;; It's possible to read-in multiple sources to a Cascalog query, and
+;; join on specific variables.
+#_(let [source-1 [["a" 1]
+                  ["a" 2]
+                  ["a" 3]
+                  ["b" 2]
+                  ["b" 4]
+                  ["c" 3]]
+        source-2 [["a" :foo]
+                  ["b" :bar]
+                  ["c" :baz]]]
     (?- (stdout)
-        (c/fixed-sample (<- [?label ?max-freq]
-                            (source ?label _ ?freq)
-                            (c/max ?freq :> ?max-freq))
-                        10)))
-
-
+        (<- [?x ?y ?z]
+            (source-1 ?x ?y)
+            (source-2 ?x ?z))))
