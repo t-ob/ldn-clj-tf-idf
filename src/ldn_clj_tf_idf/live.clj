c0278671d25efed3cd3e20604dd961fb1731f9b7
Tom O'Brien <thomas.obrien86@gmail.com>
2013-06-30 19:25:00 +0100

    Document word frequency

--- src/ldn_clj_tf_idf/live.clj
+++ src/ldn_clj_tf_idf/live.clj
@@ -5,16 +5,26 @@
 #_(require '[cascalog.playground :refer :all])
 #_(bootstrap-emacs)
 
-;; Cascalog speaks in tuples; the input to this query is a sequence of
-;; six 1-tuples. Cascalog comes with some built-in aggregators, the
-;; most simple being count, in the cascalog.ops namespace.
-#_(let [source [["Hello"]
-                ["London"]
-                ["Clojurians"]
-                ["and"]
-                ["Clojurians"]
-                ["worldwide"]]]
-  (?- (stdout)
-      (<- [?word ?count]
-          (source ?word)
-          (c/count :> ?count))))
+;; The documents we'll be working with will be formatted as a
+;; collection of 2-tuples; the first entry being the document label,
+;; and the second being a word from the document. Each word in a
+;; document corresponds to a 2-tuple.
+#_(let [source [["doc1" "screwed"]
+                ["doc1" "down"]
+                ["doc1" "hairdo"]
+                ["doc2" "thinks"]
+                ["doc2" "he'll"]
+                ["doc2" "blow"]
+                ["doc2" "our"]
+                ["doc3" "is"]
+                ["doc3" "a"]
+                ["doc3" "saddening"]
+                ["doc3" "bore"]
+                ["doc3" "and"]
+                ["doc4" "gnome"]
+                ["doc4" "and"]
+                ["doc4" "you"]]]
+    (?- (stdout)
+        (<- [?label ?word ?freq]
+            (source ?label ?word)
+            (c/count :> ?freq))))
